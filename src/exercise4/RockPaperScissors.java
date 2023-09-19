1a35a1ad9675dcedd59bf17aa627a512303b9afb
PHOTON <PHOTON@172.22.16.93>
2023-09-19 15:29:21 +0800

    start menu

--- src/exercise4/RockPaperScissors.java
+++ src/exercise4/RockPaperScissors.java
@@ -5,22 +5,54 @@
  */
 package exercise4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author PHOTON
  */
 public class RockPaperScissors{
 	public static void main(String[] args){
-		Move rock = new Move("Rock");
-		Move paper = new Move("Paper");
-		Move scissors = new Move("Scissors");
-		
-		rock.setStrongAgainst(scissors);
-		paper.setStrongAgainst(rock);
-		scissors.setStrongAgainst(paper);
-		
-		int roundsToWin = 2;
-		
-		
-	}
+            
+            Scanner sc = new Scanner(System.in);
+            String repeat = "y";
+
+            Move rock = new Move("Rock");
+            Move paper = new Move("Paper");
+            Move scissors = new Move("Scissors");
+
+            rock.setStrongAgainst(scissors);
+            paper.setStrongAgainst(rock);
+            scissors.setStrongAgainst(paper);
+
+            int roundsToWin = 2;
+            int playerScore = 0;
+            int computerScore = 0;
+
+            while(repeat.equalsIgnoreCase("y")){
+            System.out.println("Welcome to Rock, Paper, Scissors. Please choose an option:");
+            System.out.println("1. Start Game");
+            System.out.println("2. Change number of Rounds");
+            System.out.println("3. Exit Application"); 
+
+            String menuchoice = sc.nextLine();
+
+            //Start Game
+            if(menuchoice.equals(1)){
+
+            }
+
+            //Change number of Rounds
+            if(menuchoice.equals(2)){
+
+            }
+
+            //Exit Application
+            if(menuchoice.equals(3)){
+                System.out.println("Thank you for playing!");
+                repeat = "n";
+            }
+
+            }
+	}      
 }
