d49f2f716acba44888a7c04812f1ff73a020b9ef
josue <josue@Josue-Laptop>
2023-09-23 13:49:48 +0800

    moves+comparing

--- src/exercise4/RockPaperScissors.java
+++ src/exercise4/RockPaperScissors.java
@@ -30,50 +30,85 @@ public class RockPaperScissors{
             int computerScore = 0;
 
             while(repeat!=3){
-            System.out.println("Welcome to Rock, Paper, Scissors. Please choose an option:");
-            System.out.println("1. Start Game");
-            System.out.println("2. Change number of Rounds");
-            System.out.println("3. Exit Application"); 
+                System.out.println("Welcome to Rock, Paper, Scissors. Please choose an option:");
+                System.out.println("1. Start Game");
+                System.out.println("2. Change number of Rounds");
+                System.out.println("3. Exit Application"); 
 
-            String menuchoice = sc.nextLine();
-            
+                String menuchoice = sc.nextLine();
 
-            //Start Game
-            if(menuchoice.equals("1")){
-                while((playerScore!=roundsToWin)&&(computerScore!=roundsToWin)){
+
+                //Start Game
+                if(menuchoice.equals("1")){
                     System.out.println("This match will be first to "+roundsToWin+" wins.");
-                    System.out.println("The computer has selected its move. Select your move:");
-                    System.out.println("1. Rock");
-                    System.out.println("2. Paper");
-                    System.out.println("3. Scissors"); 
-                    int moveChoice = Integer.parseInt(sc.nextLine());
-                    
-                    if(moveChoice==1){
+                    playerScore = 0;
+                    computerScore = 0;
+                    while((playerScore!=roundsToWin)&&(computerScore!=roundsToWin)){
+                        
+                        int computerMove = (int) Math.floor(Math.random()*3) + 1;
+                        System.out.println("The computer has selected its move. Select your move:");
+                        System.out.println("1. Rock");
+                        System.out.println("2. Paper");
+                        System.out.println("3. Scissors"); 
+                        int result;
+                        int moveChoice = Integer.parseInt(sc.nextLine());
+
+                        Move computerMoveObject=rock;
+                        Move playerMoveObject=rock;
+                        
+                        switch(moveChoice){
+                            case 1: playerMoveObject=rock;
+                                    break;
+                            case 2: playerMoveObject=scissors;
+                                    break;
+                            case 3: playerMoveObject=paper;
+                                    break;
+                        }
+                        switch(computerMove){
+                            case 1: computerMoveObject=rock;
+                                    break;
+                            case 2: computerMoveObject=scissors;
+                                    break;
+                            case 3: computerMoveObject=paper;
+                                    break;
+                        }
+                        result = Move.compareMoves(playerMoveObject,computerMoveObject);
+                        System.out.print("Player chose "+playerMoveObject.getName()+". Computer chose "+computerMoveObject.getName()+". ");
+                        switch(result){
+                            case 0: System.out.println("Player wins round!");
+                                    playerScore ++;
+                                    break;
+                            case 1: System.out.println("Computer wins round!");
+                                    computerScore ++;
+                                    break;
+                            case 2: System.out.println("Round is tied!");
+                                    break;
+                        }
+                        System.out.println("Player: "+playerScore+" - Computer: "+computerScore);
                         
                     }
-                    if(moveChoice==2){
-                        
+                    if(playerScore==roundsToWin){
+                            System.out.println("\nPlayer Wins!\n");
                     }
-                    if(moveChoice==3){
-                        
+                    if(computerScore==roundsToWin){
+                            System.out.println("\nComputer Wins!\n");
                     }
                 }
-            }
 
-            //Change number of Rounds
-            if(menuchoice.equals("2")){
-                System.out.println("How many wins are needed to win a match?");
-                int roundChoice = Integer.parseInt(sc.nextLine());
-                roundsToWin = roundChoice;
-                System.out.println("New setting has been saved!");
-                
-            }
+                //Change number of Rounds
+                if(menuchoice.equals("2")){
+                    System.out.println("How many wins are needed to win a match?");
+                    int roundChoice = Integer.parseInt(sc.nextLine());
+                    roundsToWin = roundChoice;
+                    System.out.println("New setting has been saved!");
 
-            //Exit Application
-            if(menuchoice.equals("3")){
-                System.out.println("Thank you for playing!");
-                repeat = 3;
-            }
+                }
+
+                //Exit Application
+                if(menuchoice.equals("3")){
+                    System.out.println("Thank you for playing!");
+                    repeat = 3;
+                }
 
             }
 	}      
